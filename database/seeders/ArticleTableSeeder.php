b2fe769a78ebc70cdf158fe0372ee8f6c22f49df
Masaya Yamamoto <masaya1122baseball@gmail.com>
2024-10-24 09:15:58 +0900

    一覧と新規登録のApiを作成

---
+++ database/seeders/ArticleTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Article;
+use Illuminate\Support\Facades\Artisan;
+
+class ArticleTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        Article::factory()
+            ->count(100)
+            ->create();
+    }
+}
