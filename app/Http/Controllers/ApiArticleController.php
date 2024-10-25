1975825fae2b8baa3fd9a9daa71ce7087975e590
Masaya Yamamoto <masaya1122baseball@gmail.com>
2024-10-25 09:28:22 +0900

    API修正

--- app/Http/Controllers/ApiArticleController.php
+++ app/Http/Controllers/ApiArticleController.php
@@ -10,10 +10,16 @@ class ApiArticleController extends Controller
     /**
      * Display a listing of the resource.
      */
+    protected $articles;
+
+    public function __construct (Article $articles) {
+        $this->articles = $articles;
+    }
+
     public function index()
     {
         //
-        $articles = Article::all();
+        $articles = $this->articles->latest()->get();
         return response()->json([
             'status' => true,
             'articles' => $articles
@@ -23,10 +29,6 @@ class ApiArticleController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
@@ -34,7 +36,14 @@ class ApiArticleController extends Controller
     public function store(Request $request)
     {
         //
-        $article = Article::create($request->all());
+        $validated = $request->validate([
+            'title' => 'required',
+            'body' => 'required',
+            'status' => 'required'
+        ]);
+
+        // バリデーションされたデータを使って保存する
+        $article = Article::create($validated);
 
         return response()->json([
             'status' => true,
@@ -46,7 +55,7 @@ class ApiArticleController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Article $article)
+    public function show($id)
     {
         //
     }
@@ -54,24 +63,41 @@ class ApiArticleController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Article $article)
-    {
-        //
-    }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Article $article)
+    public function update(Request $request, $id)
     {
-        //
+        $article = Article::find($id);  
+        $article->update([  
+            "title" => $request->title,  
+            "body" => $request->body,  
+            "status" => $request->status,  
+        ]); 
+
+        return response()->json([
+            'status' => true,
+            'message' => "Article Created successfully!",
+            'article' => $article
+        ], 200);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Article $article)
+    public function destroy($id)
     {
         //
+        try {
+            $article = Article::find($id);  
+            $article->delete();
+            return response()->json([
+                'message' => "Article deleted successfully!",
+            ], 200);
+        } catch (\Exception $e) {
+            abort(406);
+        }
+
     }
 }
